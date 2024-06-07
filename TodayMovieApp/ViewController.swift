5f55e0375b4f2c569e4fa93429697044b1388f4f
soyung sim <simsy1024@gmail.com>
2024-06-07 15:43:29 +0900

    Initial Commit

---
+++ TodayMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodayMovieApp
+//
+//  Created by 심소영 on 6/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
